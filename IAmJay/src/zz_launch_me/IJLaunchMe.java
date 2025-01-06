4d08b7e190d33a50ce0424caff31abd2b54158f1
User <User@192.168.1.69>
2025-01-06 12:20:24 +0100

    a

--- IAmJay/src/zz_launch_me/IJLaunchMe.java
+++ IAmJay/src/zz_launch_me/IJLaunchMe.java
@@ -4,6 +4,9 @@ import gui.IJGuiManager;
 
 public class IJLaunchMe {
 
+	/**
+	 * Launch me
+	 */
 	public static void main(String[] _sArgs) {
 		if (_sArgs.length > 0) {
 			GUIStatic.FORCE_PATH_JAVA(_sArgs[0]);
